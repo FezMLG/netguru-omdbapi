67f2838358df9dca5b0cfdaa0e20254a72551822
FezMLG <46359181+FezMLG@users.noreply.github.com>
2021-12-21 18:29:56 +0100

    updating docker to work

--- movie-service/src/index.js
+++ movie-service/src/index.js
@@ -1,37 +1,22 @@
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
 const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
 const mongoose = require("mongoose");
 
 require("dotenv/config");
 const moviesRouter = require("./routes/movies");
+const swagger = require("./config/api-docs");
+const cors = require("./config/cors");
+
 const PORT = process.env.PORT || 4000;
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-    },
-    servers: [
-      {
-        url: "http://localhost:4000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-const specs = swaggerJsDoc(options);
+
 const app = express();
 
-app.use(cors());
+app.use(cors);
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/api/api-docs", swaggerUI.serve, swaggerUI.setup(swagger));
 app.use("/api/movies", moviesRouter);
 
 mongoose.connect(process.env.MONGODB_URI || process.env.DB_CONNECTION, () =>
