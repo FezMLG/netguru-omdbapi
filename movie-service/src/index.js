d6256cbbbf83069408539d7b1e658bd246a87b99
FezMLG <46359181+FezMLG@users.noreply.github.com>
2021-12-21 20:58:52 +0100

    Update index.js

--- movie-service/src/index.js
+++ movie-service/src/index.js
@@ -11,7 +11,7 @@ const swagger = require("./config/api-docs");
 
 const PORT = process.env.PORT || 4000;
 
-const app express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
