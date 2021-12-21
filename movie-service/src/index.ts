05cca1aa9c5c9c7bd15b4e775e1ddc148e422f5b
FezMLG <46359181+FezMLG@users.noreply.github.com>
2021-12-21 17:22:24 +0100

    more updates

--- movie-service/src/index.ts
+++ movie-service/src/index.ts
@@ -1,43 +1,36 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
+import logger from 'morgan';
 import { serve, setup } from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from 'swagger-ui-express';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import swagger from './config/api-docs';
+import moviesRouter from "./routes/movies";
+import endpoint from './config/endpoints.config';
 
 dotenv.config();
 
-import moviesRouter from "./routes/movies";
 const PORT = process.env.PORT || 4000;
-const options: any = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-    },
-    servers: [
-      {
-        url: "http://localhost:4000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-const specs = swaggerJsDoc(options);
+
 const app = express();
 
 app.use(cors);
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(logger('dev'));
 
-app.use("/api-docs", serve, setup(specs));
+app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
+
 app.use("/api/movies", moviesRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(""));
 }
+
+mongoose.connect(endpoint.DB_CONNECTION, () =>
+  console.log("Connected to mongodb")
+);
 
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
 
--- movie-service/src/routes/movies.ts
+++ movie-service/src/routes/movies.ts
@@ -8,72 +8,8 @@ import checkForLimit from "../middleware/checkForLimit";
 
 require("dotenv").config();
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Movie:
- *       type: object
- *       required:
- *         - Title
- *         - UserID
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the movie
- *         Title:
- *           type: string
- *           description: The movie title
- *         Released:
- *           type: date
- *           description: The movie release date
- *         Genre:
- *           type: string
- *           description: The movie genre
- *         Director:
- *           type: string
- *           description: The movie author
- *         UserID:
- *           type: string
- *           description: ID of the user that added this movie
- *         createdAt:
- *           type: date
- *           description: Date that the movie was created by the user (ms)
- *       example:
- *         id: qwerty
- *         title: Example title
- *         released: 1639661479674
- *         genre: Example genre
- *         director: Example director
- *         userID: Example user
- *         createdAt: 1639676666395
- */
-
-/**
- * @swagger
- * tags:
- *   name: Movies
- *   description: The movies managing API
- */
-
-/**
- * @swagger
- * /movies:
- *   get:
- *     summary: Returns the list of all the movies
- *     tags: [Movies]
- *     responses:
- *       200:
- *         description: The list of the movies
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Movie'
- */
-
 router.get("/", authenticate, async (req, res) => {
+  console.log("trying");
   try {
     const movies = await Movie.find({ userID: res.locals.userId });
     res.send({
@@ -88,29 +24,6 @@ router.get("/", authenticate, async (req, res) => {
     res.send(500).send({ message: "Error occurred" });
   }
 });
-
-/**
- * @swagger
- * /movies:
- *   post:
- *     summary: Create a new movie
- *     tags: [Movies]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Movie'
- *     responses:
- *       200:
- *         description: The movie was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Movie'
- *       500:
- *         description: Some server error
- */
 
 router.post("/", authenticate, checkForLimit, async (req, res) => {
   try {
